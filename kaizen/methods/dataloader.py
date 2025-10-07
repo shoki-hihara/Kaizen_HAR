d0d8f602cb2b9421ed41c7e0a1779cb3ae3378c3
shoki-hihara <131498289+shoki-hihara@users.noreply.github.com>
2025-10-07 15:07:30 +0900

    Update dataloader.py

--- kaizen/methods/dataloader.py
+++ kaizen/methods/dataloader.py
@@ -1,31 +1,37 @@
+# my_har_dataloader.py
+import os
 import numpy as np
 import torch
-from torch.utils.data import TensorDataset, DataLoader
+from torch.utils.data import Dataset, DataLoader
 
-def load_wisdm_dataset(data_dir: str, split: str = "train"):
-    """
-    前処理済み WISDM2019 データセットを読み込む
+class WISDMDataset(Dataset):
+    def __init__(self, data_path, split="train"):
+        """
+        data_path: ディレクトリ。中に train_X.npy, train_y.npy, test_X.npy, test_y.npy がある
+        split: "train" or "test"
+        """
+        if split == "train":
+            self.X = np.load(os.path.join(data_path, "train_X.npy"))
+            self.y = np.load(os.path.join(data_path, "train_y.npy"))
+        else:
+            self.X = np.load(os.path.join(data_path, "test_X.npy"))
+            self.y = np.load(os.path.join(data_path, "test_y.npy"))
 
-    Args:
-        data_dir (str): データセット格納ディレクトリ
-        split (str): "train" or "test"
+        # torch tensor に変換
+        self.X = torch.tensor(self.X, dtype=torch.float32)
+        self.y = torch.tensor(self.y, dtype=torch.long)
 
-    Returns:
-        dataset (TensorDataset): TensorDataset (X, y)
-    """
-    X_path = f"{data_dir}/{split}_X.npy"
-    y_path = f"{data_dir}/{split}_y.npy"
+    def __len__(self):
+        return len(self.y)
 
-    X = np.load(X_path).astype(np.float32)
-    y = np.load(y_path).astype(np.int64)
+    def __getitem__(self, idx):
+        # X の shape は (channel, timesteps) を想定
+        return self.X[idx], self.y[idx]
 
-    # HAR用に (batch, channel, timestep) 形状に変換
-    if X.ndim == 3:  # (batch, timestep, channel)
-        X = X.transpose(0, 2, 1)
 
-    dataset = TensorDataset(torch.from_numpy(X), torch.from_numpy(y))
-    return dataset
+def load_wisdm_dataset(data_dir, split="train"):
+    return WISDMDataset(data_dir, split=split)
 
-def get_dataloader(data_dir: str, split: str = "train", batch_size: int = 64, shuffle: bool = True, num_workers: int = 2):
-    dataset = load_wisdm_dataset(data_dir, split)
+
+def get_dataloader(dataset, batch_size=64, shuffle=True, num_workers=2):
     return DataLoader(dataset, batch_size=batch_size, shuffle=shuffle, num_workers=num_workers)
