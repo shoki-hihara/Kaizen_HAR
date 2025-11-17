3cb3caa378f9357fc2bd066196baa14d092a71d3
shoki-hihara <131498289+shoki-hihara@users.noreply.github.com>
2025-11-17 12:26:30 +0900

    Create main_linear.py

---
+++ bash_files/main_linear.py
@@ -0,0 +1,25 @@
+python3 main_linear.py \
+    --dataset wisdm2019 \
+    --encoder tpn \
+    --data_dir $DATA_DIR \
+    --split_strategy class \
+    --num_classes 18 \
+    --num_tasks 6 \
+    --task_idx ${TASK} \
+    --input_dim 3 \
+    --feature_dim 128 \
+    --num_layers 2 \
+    --hidden_dim 256 \
+    --dropout 0.1 \
+    --batch_size 256 \
+    --num_workers 2 \
+    --optimizer sgd \
+    --lr 0.1 \
+    --weight_decay 1e-4 \
+    --scheduler cosine \
+    --pretrained_feature_extractor $CKPT \
+    --name wisdm-linear-task${TASK} \
+    --project Kaizen_HAR_LINEAR \
+    --entity crazy-sonnet-ambl \
+    --wandb \
+    --checkpoint_dir ./experiments/linear_eval
